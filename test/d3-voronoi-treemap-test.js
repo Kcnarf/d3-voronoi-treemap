5a52db488cb3c12311d8f0c4183df4fe01816a35
Franck Lebeau <fl.franck.lebeau+github@gmail.com>
2017-12-01 11:11:16 +0100

    add API tests

--- test/d3-voronoi-treemap-test.js
+++ test/d3-voronoi-treemap-test.js
@@ -2,13 +2,55 @@ var tape = require("tape"),
     d3VoronoiTreemap = require('../build/d3-voronoi-treemap');
 
 tape("voronoiTreemap(...) should set the expected defaults", function(test) {
-var voronoiTreemap = d3VoronoiTreemap.voronoiTreemap(),
-    datum = {weight: 3};
+  var voronoiTreemap = d3VoronoiTreemap.voronoiTreemap(),
+      datum = {weight: 1};
 
-test.equal(voronoiTreemap.weight()(datum), 3);
-test.equal(voronoiTreemap.convergenceTreshold(), 0.01);
-test.equal(voronoiTreemap.maxIterationCount(), 50);
-test.equal(voronoiTreemap.nearZeroWeightRatio(), 0.01);
-test.deepEqual(voronoiTreemap.clip(), [[0,0], [0,1], [1,1], [1,0]]);
-test.end();
+  test.equal(voronoiTreemap.weight()(datum), 1);
+  test.equal(voronoiTreemap.convergenceTreshold(), 0.01);
+  test.equal(voronoiTreemap.maxIterationCount(), 50);
+  test.equal(voronoiTreemap.nearZeroWeightRatio(), 0.01);
+  test.deepEqual(voronoiTreemap.clip(), [[0,0], [0,1], [1,1], [1,0]]);
+  test.end();
+});
+
+tape("voronoiTreemap.weight(...) should set the specified weight-accessor", function(test) {
+  var voronoiTreemap = d3VoronoiTreemap.voronoiTreemap(),
+      datum = {weight: 1, weightPrime: 2};
+
+  test.equal(voronoiTreemap.weight(function(d){ return d.weightPrime; }), voronoiTreemap);
+  test.equal(voronoiTreemap.weight()(datum), 2);
+  test.end();
+});
+  
+tape("voronoiTreemap.clip(...) should set the adequate convex, hole-free, counterclockwise clipping polygon", function(test) {
+  var voronoiTreemap = d3VoronoiTreemap.voronoiTreemap(),
+      newClip = [[0,0], [0,1], [1,0], [1,1]];   //self-intersecting polygon
+
+  test.equal(voronoiTreemap.clip(newClip), voronoiTreemap);
+  test.deepEqual(voronoiTreemap.clip(), [[1,1], [1,0], [0,0], [0,1]]);
+  test.end();
+});
+
+tape("voronoiTreemap.convergenceTreshold(...) should set the specified convergence treshold", function(test) {
+  var voronoiTreemap = d3VoronoiTreemap.voronoiTreemap();
+
+  test.equal(voronoiTreemap.convergenceTreshold(0.001), voronoiTreemap);
+  test.equal(voronoiTreemap.convergenceTreshold(), 0.001);
+  test.end();
+});
+
+tape("voronoiTreemap.maxIterationCount(...) should set the specified allowed number of iterations", function(test) {
+  var voronoiTreemap = d3VoronoiTreemap.voronoiTreemap();
+
+  test.equal(voronoiTreemap.maxIterationCount(100), voronoiTreemap);
+  test.equal(voronoiTreemap.maxIterationCount(), 100);
+  test.end();
+});
+
+tape("voronoiTreemap.nearZeroWeightRatio(...) should set the specified ratio", function(test) {
+  var voronoiTreemap = d3VoronoiTreemap.voronoiTreemap();
+
+  test.equal(voronoiTreemap.nearZeroWeightRatio(0.001), voronoiTreemap);
+  test.equal(voronoiTreemap.nearZeroWeightRatio(), 0.001);
+  test.end();
 });
